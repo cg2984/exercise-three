610cb19fa5e7dfde5faf88f08a9d8336efbed468
Clara Gilligan <31659760+cg2984@users.noreply.github.com>
2020-03-25 18:39:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-//importing the routes by requiring it 
+//importing the routes by requiring it and linking it to the app.use when routing
 const indexRoute = require('./routes/index.js');
 const aboutRoute = require('./routes/about.js');
 
-//the function recieves two arguements, the path and the arrow function
+//the function recieves two arguements, the path and the callback function ( which is an arrow function in this case)
 //req = request res = response
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -17,10 +17,9 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.use('/static', express.static("public"));
 
 //-----------Routing in Express--------//
-//the arrow part is a call back function and it need that
+
 app.use('/', indexRoute);
 app.use('/about', aboutRoute);
 
-//function is express object 
 //the two arguements are the port you have and a console log
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
